7c6419699419b6cab38106bd9871826743887332
Thiago Centurion <thiagocenturion@me.com>
2021-08-30 23:20:22 -0300

    Added SwiftyJSON dependency to the project

---
+++ Bored/Bored/Scenes/Home/HomeCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  HomeCoordinator.swift
+//  Bored
+//
+//  Created by MACBOOK on 30/08/21.
+//
+
+import Foundation
---
+++ Bored/Bored/Scenes/Home/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  Bored
+//
+//  Created by MACBOOK on 30/08/21.
+//
+
+import Foundation
